71c21ca84b1a105f12b2c5c73a2e8136e6c4d369
Tranfu <tranfu@qq.com>
2024-06-19 08:09:01 +0800

    create my app

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import * as React from "react";
+import { Routes, Route, Outlet, Link, useNavigate } from "react-router-dom";
+
+// import logo from './logo.svg';
+// import './App.css';
+import ProLayout from './layouts/ProLayout';
+
+const Home = React.lazy(() => import('./pages/Home'));
+const NoMatch = React.lazy(() => import('./pages/NoMatch'));
+const About = React.lazy(() => import('./pages/About'));
+const Dashboard = React.lazy(() => import('./pages/Dashboard'));
+
+function App() {
+  return (
+    <div className="App">
+      {/* https://github.com/remix-run/react-router/blob/dev/examples/lazy-loading/src/App.tsx */}
+      <Routes>
+        <Route path="/" element={<ProLayout />}>
+          <Route index element={<Home />} />
+          <Route
+            path="home"
+            element={
+              <React.Suspense fallback={<>Loading...</>}>
+                <Home />
+              </React.Suspense>
+            }
+          />
+          <Route
+            path="about"
+            element={
+              <React.Suspense fallback={<>Loading...</>}>
+                <About />
+              </React.Suspense>
+            }
+          />
+          <Route
+            path="dashboard/*"
+            element={
+              <React.Suspense fallback={<>Loading...</>}>
+                <Dashboard />
+              </React.Suspense>
+            }
+          />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, HashRouter } from "react-router-dom";
+
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* https://reactrouter.com/en/main/router-components/hash-router */}
+    {/* https://reactrouter.com/en/main/upgrading/future#v7_starttransition */}
+    {/* https://github.com/remix-run/react-router/blob/dev/examples/lazy-loading/src/main.tsx */}
+    <BrowserRouter future={{ v7_startTransition: true }}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
