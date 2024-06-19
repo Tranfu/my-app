588e9ab9ddb39036fa6ebe7c3b92287339505310
Tranfu <tranfu@qq.com>
2024-06-19 22:48:30 +0800

    fix code formatter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
@@ -12,10 +12,10 @@ root.render(
     {/* https://reactrouter.com/en/main/router-components/hash-router */}
     {/* https://reactrouter.com/en/main/upgrading/future#v7_starttransition */}
     {/* https://github.com/remix-run/react-router/blob/dev/examples/lazy-loading/src/main.tsx */}
-    <BrowserRouter future={{ v7_startTransition: true }}>
-      <App />
+    <BrowserRouter future={{v7_startTransition: true}}>
+              <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
